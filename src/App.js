155fe43a425314090fdb9f6ff08af88b455626be
olegri28 <oleg.ri@moonactive.com>
2022-01-23 11:06:58 +0200

    added cart visibility state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHeandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler() {
+    showCartHeandler(false);
+  }
+
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHeandler} />
       <main>
         <Meals />
       </main>
